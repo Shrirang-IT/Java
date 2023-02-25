7a654c6788d334ede2d3c961a06e2e61f772edd4
Shrirang <sbbhalerao.it@gmail.com>
2023-02-25 23:10:48 +0530

    Loops and If else

---
+++ src/Loops.java
@@ -0,0 +1,13 @@
+
+
+public class Loops {
+
+    boolean var = true;
+
+    void check () {
+        if (var){
+            System.out.println("The value is true");
+        }
+        else { System.out.println("THe above statement is false"); }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ public class Main  {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         HelloWorld helloWorld = new HelloWorld();
+        Loops loops =new Loops();
         helloWorld.print();
         System.out.println(helloWorld.num);
+        loops.check();
 
     }
 }
