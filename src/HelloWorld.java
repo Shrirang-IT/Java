ea7cfa6e04f42891780318a36177db87e94c9815
Shrirang <sbbhalerao.it@gmail.com>
2023-02-25 17:46:15 +0530

    Initial version

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+    protected int num =100;
+    void print()
+    {
+        System.out.println("Hello Shrirang");
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main  {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.print();
+        System.out.println(helloWorld.num);
+
+    }
+}
